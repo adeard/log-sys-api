78f805fe8db0a8011d1676c1c3ffa81915de50d3
adeard <adeardian1994@gmail.com>
2025-02-21 14:29:30 +0700

    add app name

--- utils/logger.go
+++ utils/logger.go
@@ -23,7 +23,13 @@ func LogInit(host string, message string) {
 	log.Println(message)
 }
 
+func firstDir(permalink string) string {
+	split := strings.Split(permalink, string(os.PathSeparator))
+	return split[1]
+}
+
 func LogInit2(host string, message domain.LogRequest) {
+	appName := "unnamed"
 	now := time.Now().Format("2006-01-02")
 
 	if !strings.HasPrefix(host, "http://") && !strings.HasPrefix(host, "https://") {
@@ -35,8 +41,14 @@ func LogInit2(host string, message domain.LogRequest) {
 		LogInit("error", err.Error())
 	}
 
+	split := strings.Split(parsedURL.Path, "/")
+
+	if len(split) > 0 {
+		appName = split[1]
+	}
+
 	hostRes := strings.ReplaceAll(parsedURL.Host, ":", "@")
-	filename := hostRes + "_" + now + ".log"
+	filename := hostRes + "_" + appName + "_" + now + ".log"
 
 	f, err := os.OpenFile("./logs/"+filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
